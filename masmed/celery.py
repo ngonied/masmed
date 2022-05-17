d8cbe079ddd399bd28727495b3fbb41609b08dba
ngonied <ngoniechizororo@gmail.com>
2022-05-17 10:08:55 +0200

    Removed celery

--- masmed/celery.py
+++ masmed/celery.py
@@ -1,9 +1,9 @@
-#from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'masmed.settings')
-app = Celery('masmed', broker='amqps://admin:1234@127.0.0.1:5672/ngonied_host?ssl=true')
-print(app.conf.broker_url)
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+# #from __future__ import absolute_import, unicode_literals
+# import os
+# from celery import Celery
+# # set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'masmed.settings')
+# app = Celery('masmed', broker='amqps://admin:1234@127.0.0.1:5672/ngonied_host?ssl=true')
+# print(app.conf.broker_url)
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
--- masmed/settings.py
+++ masmed/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 465 #587
-EMAIL_HOST_USER = "ngoniechizororo@gmail.com"
-EMAIL_HOST_PASSWORD = "nyikadzino14083"
+EMAIL_HOST_USER = "chizirian@gmail.com"
+EMAIL_HOST_PASSWORD = "<>"
